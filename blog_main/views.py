423a884897da8b9969413dbfc0410c43becc3a0d
neerajdevs <neerajgupta150019@gmail.com>
2025-10-28 09:44:33 +0530

    Add Search Functionality

--- blog_main/views.py
+++ blog_main/views.py
@@ -3,6 +3,7 @@ from .models import *
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from .context_processors import *
+from django.db.models import Q # Q object import karna zaroori hai
 
 # Create your views here.
 
@@ -28,3 +29,16 @@ def search_blogs(request , slug):
     post = get_object_or_404(Blogs , slug = slug , status = 'publish')
     # category = Category.objects.all() # now it get by context [processors]
     return render(request , 'view_post.html' , {'blog' : post } )
+
+def search(request):
+    keyword = request.GET.get('keyword')  
+    if keyword:
+        # Q objects ka use karke 'OR' condition banao
+        queryset = Blogs.objects.filter(
+            Q(title__icontains=keyword) |       
+            Q(author__username__icontains=keyword) , status = 'publish'  
+        )
+    else:
+        # Agar koi keyword nahi hai, toh empty queryset ya saare blogs return kar sakte hain
+        queryset = Blogs.objects.none() # Ya Blogs.objects.all() agar sab dikhane hain
+    return render(request , 'search.html' , {'results' : queryset})
